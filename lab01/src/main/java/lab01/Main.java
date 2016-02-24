ed223be906d90994fbd3cd2c3b0dce8d1f79b80e
Lukasz Jercha <L.Jercha@kainos.com>
2016-02-24 22:16:22 +0100

    Added project with travis CI configuration file.

---
+++ lab01/src/main/java/lab01/Main.java
@@ -0,0 +1,19 @@
+package lab01;
+
+public class Main {
+
+	public static long heapSize = 0;
+	
+	public static void main(String[] args) {
+		int i = 0;
+		
+		BigBoom(i);
+	}
+	
+	public static void BigBoom(int n) {
+		int[][] tab = new int[1000][1000];
+		heapSize += 1000000 * 4; // int = 32 bits
+		System.out.println(n + ". Heap size(bytes):" + heapSize);
+		BigBoom(n + 1);
+	}
+}
